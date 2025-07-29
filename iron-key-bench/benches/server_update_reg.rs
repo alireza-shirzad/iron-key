44cb7e7b5db2351008d93fd05419200835889349
Alireza Shirzad <alr.shirzad@gmail.com>
2025-07-28 20:08:34 -0400

    Refactor code structure for improved readability and maintainability

--- iron-key-bench/benches/server_update_reg.rs
+++ iron-key-bench/benches/server_update_reg.rs
@@ -120,7 +120,7 @@ pub const PARAMS: &[Params] = &{
         let mut i: usize = 0;
 
         let mut n: usize = 20; // log_capacity starts from 20
-        while n <= 32 {
+        while n <= 31 {
             let mut k = 10; // log_update_size
             while k <= n - 2 {
                 if i < PARAMS_ARRAY_SIZE {
